0296bc72e31150f3d9f731aec98149f252404200
Dan McGrath <daniel.mcgrath9@gmail.com>
2018-04-08 14:56:36 -0400

    refactor js into one file, add new sounds

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
-
-function playSoundOnKeyPress(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
-  const item = document.querySelector(`.item[data-key="${e.keyCode}"]`);
-  if (!audio) return; // stop the function from running all together
-  audio.currentTime = 0;
-  audio.play();
-  item.classList.add("playing");
-
+(function() {
+  // code for allowing clicks
   const items = document.querySelectorAll(".item");
   items.forEach(item =>
-    item.addEventListener("transitionend", removeTransition)
+    item.addEventListener("click", function(e) {
+      const key = item.dataset.key;
+      const audio = document.querySelector(`audio[data-key="${key}"`);
+      if (!audio) return;
+      audio.currentTime = 0;
+      audio.play();
+      item.classList.add("playing");
+
+      item.addEventListener("transitionend", removeTransition);
+    })
   );
-}
 
-function removeTransition(e) {
-  if (e.propertyName !== "transform") return;
+  function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
 
-  this.classList.remove("playing");
-}
+    this.classList.remove("playing");
+  }
 
-window.addEventListener("keydown", playSoundOnKeyPress);
+  function playSoundOnKeyPress(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+    const item = document.querySelector(`.item[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    item.classList.add("playing");
+  
+    const items = document.querySelectorAll(".item");
+    items.forEach(item =>
+      item.addEventListener("transitionend", removeTransition)
+    );
+  }
+  
+  window.addEventListener("keydown", playSoundOnKeyPress);
+
+})();
+
+
