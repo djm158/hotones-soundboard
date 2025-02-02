dd65044c28d9092a193a3d90720d936d1eaddd65
Dan McGrath <daniel.mcgrath9@gmail.com>
2025-02-01 21:14:06 -0500

    WIP

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-(function() {
+const app = (function () {
   // code for allowing clicks
   const items = document.querySelectorAll(".item");
-  items.forEach(item =>
-    item.addEventListener("click", function(e) {
+  items.forEach((item) =>
+    item.addEventListener("click", function (e) {
       const key = item.dataset.key;
       const audio = document.querySelector(`audio[data-key="${key}"`);
       if (!audio) return;
@@ -27,15 +27,59 @@
     audio.currentTime = 0;
     audio.play();
     item.classList.add("playing");
-  
+
     const items = document.querySelectorAll(".item");
-    items.forEach(item =>
+    items.forEach((item) =>
       item.addEventListener("transitionend", removeTransition)
     );
   }
-  
+
   window.addEventListener("keydown", playSoundOnKeyPress);
 
+  // handle right click on key box to open keybinding modal
+  const keyBoxes = document.querySelectorAll(".key-box");
+  keyBoxes.forEach((keyBox) => {
+    keyBox.addEventListener("contextmenu", function (e) {
+      console.log("contextmenu");
+      const key = keyBox.textContent;
+      const text = keyBox.nextElementSibling.textContent;
+      e.preventDefault();
+      if (
+        document.querySelector(".key-binding-modal").classList.contains("open")
+      ) {
+        closeModal();
+      } else {
+        openModal(key, text);
+      }
+    });
+  });
+
+  function openModal(key, text) {
+    document.querySelector(".key-binding-modal").classList.add("open");
+    document.querySelector(
+      ".key-binding-modal-title"
+    ).textContent = `Configure Key Binding for: ${text}`;
+    document.querySelector(".key-binding-modal-key").textContent = key;
+  }
+
+  function closeModal() {
+    document.querySelector(".key-binding-modal").classList.remove("open");
+  }
+  function saveKeyBinding() {
+    const key = document.querySelector(".key-binding-modal-input").value;
+
+    const keyCode = key.toUpperCase().charCodeAt(0);
+    console.log(keyCode);
+    const keybinding = document.querySelector(`[data-key="${keyCode}"]`);
+    console.log(keybinding);
+    if (!keybinding) return;
+    keybinding.children[0].textContent = key.toUpperCase();
+    document.querySelector(".key-binding-modal-input").value = "";
+    closeModal();
+  }
+  return {
+    saveKeyBinding,
+    openModal,
+    closeModal,
+  };
 })();
-
-
