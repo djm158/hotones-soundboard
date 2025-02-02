960ad2f6766973e42e9d85b78ebd790d05c4004a
Dan McGrath <daniel.mcgrath9@gmail.com>
2025-02-01 21:34:52 -0500

    fix: keyCode is deprecated (#10)
    * fix: keyCode is deprecated
    
    * refactor: event delegation (#11)

--- app.js
+++ app.js
@@ -1,41 +1,29 @@
-(function() {
-  // code for allowing clicks
-  const items = document.querySelectorAll(".item");
-  items.forEach(item =>
-    item.addEventListener("click", function(e) {
-      const key = item.dataset.key;
-      const audio = document.querySelector(`audio[data-key="${key}"`);
-      if (!audio) return;
-      audio.currentTime = 0;
-      audio.play();
-      item.classList.add("playing");
-
-      item.addEventListener("transitionend", removeTransition);
-    })
-  );
-
-  function removeTransition(e) {
-    if (e.propertyName !== "transform") return;
-
-    this.classList.remove("playing");
-  }
-
-  function playSoundOnKeyPress(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
-    const item = document.querySelector(`.item[data-key="${e.keyCode}"]`);
+(function () {
+  function playSound(key) {
+    const audio = document.querySelector(`audio[data-key="${key}"]`);
+    const item = document.querySelector(`.item[data-key="${key}"]`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
     item.classList.add("playing");
-  
-    const items = document.querySelectorAll(".item");
-    items.forEach(item =>
-      item.addEventListener("transitionend", removeTransition)
-    );
+
+    item.addEventListener("transitionend", removeTransition);
   }
-  
-  window.addEventListener("keydown", playSoundOnKeyPress);
 
+  function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+    this.classList.remove("playing");
+  }
+
+  document.querySelector(".flex-grid").addEventListener("click", function (e) {
+    if (e.target.closest(".item")) {
+      const key = e.target.closest(".item").dataset.key;
+      playSound(key);
+    }
+  });
+
+  // Handle keydown events
+  window.addEventListener("keydown", function (e) {
+    playSound(e.key.toUpperCase());
+  });
 })();
-
-
